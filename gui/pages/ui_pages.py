2b56b0969cf75dd217a328d900ba6ef8c93a05a9
Marlos_Balbino <53904084+MarlosBalbino@users.noreply.github.com>
2022-03-20 00:58:47 -0300

    limpeza de código

--- gui/pages/ui_pages.py
+++ gui/pages/ui_pages.py
@@ -1,13 +1,3 @@
-# -*- coding: utf-8 -*-
-
-################################################################################
-## Form generated from reading UI file 'untitledfgxEqq.ui'
-##
-## Created by: Qt User Interface Compiler version 6.1.2
-##
-## WARNING! All changes made in this file will be lost when recompiling UI file!
-################################################################################
-
 """ from PySide6.QtCore import *  # type: ignore
 from PySide6.QtGui import *  # type: ignore
 from PySide6.QtWidgets import *  # type: ignore """
@@ -21,49 +11,31 @@ class Ui_application_pages(object):
         if not application_pages.objectName():
             application_pages.setObjectName(u"application_pages")
         application_pages.resize(622, 515)
+        application_pages.setWindowTitle("application_pages")
+
         self.page_1 = QWidget()
-        self.page_1.setObjectName(u"page_1")
         self.verticalLayout = QVBoxLayout(self.page_1)
-        self.verticalLayout.setObjectName(u"verticalLayout")
         self.label_3 = QLabel(self.page_1)
-        self.label_3.setObjectName(u"label_3")
         self.label_3.setAlignment(Qt.AlignCenter)
-
-        self.verticalLayout.addWidget(self.label_3)
-        
+        self.label_3.setText("Divices")
+        self.verticalLayout.addWidget(self.label_3)        
         application_pages.addWidget(self.page_1)
+
         self.page_2 = QWidget()
-        self.page_2.setObjectName(u"page_2")
         self.verticalLayout_2 = QVBoxLayout(self.page_2)
-        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.label_2 = QLabel(self.page_2)
-        self.label_2.setObjectName(u"label_2")
         self.label_2.setAlignment(Qt.AlignCenter)
-
+        self.label_2.setText("ASCADA")
         self.verticalLayout_2.addWidget(self.label_2)
-
         application_pages.addWidget(self.page_2)
+
         self.page_3 = QWidget()
-        self.page_3.setObjectName(u"page_3")
         self.verticalLayout_3 = QVBoxLayout(self.page_3)
-        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
         self.label = QLabel(self.page_3)
-        self.label.setObjectName(u"label")
         self.label.setAlignment(Qt.AlignCenter)
-
+        self.label.setText("Charts")
         self.verticalLayout_3.addWidget(self.label)
-
         application_pages.addWidget(self.page_3)
 
-        self.retranslateUi(application_pages)
-
-        QMetaObject.connectSlotsByName(application_pages)
+        # QMetaObject.connectSlotsByName(application_pages)
     # setupUi
-
-    def retranslateUi(self, application_pages):
-        application_pages.setWindowTitle(QCoreApplication.translate("application_pages", u"StackedWidget", None))
-        self.label_3.setText(QCoreApplication.translate("application_pages", u"Página 1: Divices", None))
-        self.label_2.setText(QCoreApplication.translate("application_pages", u"Página_2: ASCADA", None))
-        self.label.setText(QCoreApplication.translate("application_pages", u"Página_3: Charts", None))
-    # retranslateUi
-
